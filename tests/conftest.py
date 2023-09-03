1ab867010481c1b3b1f1d39d1e38a898b87e2627
Vasilii Leontev <leovas1972@gmail.com>
2023-09-03 22:55:54 +0700

    added firfox

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,12 @@ from selene.support.shared import browser
 @pytest.fixture(scope='function')
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
+    # browser.config.driver_name = 'firefox'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     browser.config.set_value_by_js = True
     browser.config.with_(click_by_js=True)
+
+    yield
+
+    browser.quit()
