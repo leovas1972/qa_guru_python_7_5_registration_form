599475c88559bd57106f439f30c8780fa096bb54
Vasilii Leontev <leovas1972@gmail.com>
2023-09-04 17:22:55 +0700

    deleting a comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ from selene.support.shared import browser
 @pytest.fixture(scope='function')
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-    # browser.config.driver_name = 'firefox'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.set_value_by_js = True
